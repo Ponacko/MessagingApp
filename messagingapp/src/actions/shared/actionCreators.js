1f9115fd0e9ce5a108c82325e29b43375eddaaf9
Matúš Sedlák <sedlak87@gmail.com>
2017-12-05 21:53:22 +0100

    added profile functionality, api connection

--- messagingapp/src/actions/shared/actionCreators.js
+++ messagingapp/src/actions/shared/actionCreators.js
@@ -1,14 +1,27 @@
 import * as actionTypes from '../../constants/actionTypes';
-import {SHARED_RECEIVE_TOKEN} from "../../constants/actionTypes";
-import {SHARED_INVALIDATE_TOKEN} from "../../constants/actionTypes";
+import { errorActionFactory } from '../../utils/errorActionFactory';
 
 export const receiveValidToken = () => ({
-    type: SHARED_RECEIVE_TOKEN,
+    type: actionTypes.SHARED_RECEIVE_TOKEN,
     payload: {
         token: 'forged-token-much-guid-such-wow',
     }
 });
 
 export const invalidateToken = () => ({
-    type: SHARED_INVALIDATE_TOKEN,
+    type: actionTypes.SHARED_INVALIDATE_TOKEN,
 });
+
+export const startAuthentication = () => ({
+    type: actionTypes.SHARED_AUTHENTICATION_STARTED,
+});
+
+
+export const dismissError = (errorId) => ({
+    type: actionTypes.SHARED_DISMISS_ERROR,
+    payload: {
+        errorId,
+    }
+});
+
+export const failAuthentication = errorActionFactory(actionTypes.SHARED_AUTHENTICATION_FAILED);
--- messagingapp/src/components/app/ContentLayout.jsx
+++ messagingapp/src/components/app/ContentLayout.jsx
@@ -3,9 +3,9 @@ import { Route } from 'react-router-dom';
 import { Content } from './Content.jsx';
 
 const ContentLayout = () => (
-<div className>
-    <Content />
-</div>
+    <div className="row form-group">
+        <Content />
+    </div>
 );
 
 export { ContentLayout };
