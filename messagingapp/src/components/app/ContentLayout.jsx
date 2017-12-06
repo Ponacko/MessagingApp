e0e892a5ec69d53b83d371cc46391b76717b0265
Ponacko <ponacko@gmail.com>
2017-12-06 23:10:20 +0100

    Add titles to routes

--- messagingapp/src/components/app/ContentLayout.jsx
+++ messagingapp/src/components/app/ContentLayout.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Content } from './Content.jsx';
+import {Route} from 'react-router-dom';
+import {Content} from './Content.jsx';
+import {HeadInHelmet} from "../../containers-redux/shared/HeadInHelmet";
 
 const ContentLayout = () => (
 <div className>
+    <HeadInHelmet />
     <Content />
 </div>
 );
